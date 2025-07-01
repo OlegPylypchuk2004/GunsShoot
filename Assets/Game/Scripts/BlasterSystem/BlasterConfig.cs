f38a30b145faf52d8f6be9c9be7c3b7959db798d
OlegPylypchuk2004 <154813576+OlegPylypchuk2004@users.noreply.github.com>
2025-07-01 13:17:56 +0300

    Removed bullet config

--- Assets/Game/Scripts/BlasterSystem/BlasterConfig.cs
+++ Assets/Game/Scripts/BlasterSystem/BlasterConfig.cs
@@ -6,7 +6,7 @@ namespace BlasterSystem
     [CreateAssetMenu(fileName = "BlasterConfig", menuName = "Configs/Blaster")]
     public class BlasterConfig : ScriptableObject
     {
-        [field: SerializeField] public BulletConfig Bullet { get; private set; }
+        [field: SerializeField] public Bullet Bullet { get; private set; }
         [field: SerializeField, Min(1)] public int Damage { get; private set; }
         [field: SerializeField, Min(1)] public float BulletSpeed { get; private set; }
         [field: SerializeField, Min(1)] public int Ammo { get; private set; }
