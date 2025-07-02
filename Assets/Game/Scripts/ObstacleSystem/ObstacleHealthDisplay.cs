9565938948e3db5ede6e695d633e523641dd008e
OlegPylypchuk2004 <154813576+OlegPylypchuk2004@users.noreply.github.com>
2025-07-02 11:47:48 +0300

    Added obstacle health bar

--- Assets/Game/Scripts/ObstacleSystem/ObstacleHealthDisplay.cs
+++ Assets/Game/Scripts/ObstacleSystem/ObstacleHealthDisplay.cs
@@ -9,11 +9,21 @@ namespace ObstacleSystem
         [SerializeField, Min(0f)] private float _resultFillDuration;
         [SerializeField, Min(0f)] private float _resultFillDelay;
         [SerializeField] private Ease _resultFillEase;
-        [SerializeField] private Obstacle _obstacle;
+        [SerializeField] private Canvas _canvas;
         [SerializeField] private Image _currentFillImage;
         [SerializeField] private Image _resultFillImage;
 
+        private Obstacle _obstacle;
         private Tween _resultFillImageTween;
+
+        private void Awake()
+        {
+            _canvas.worldCamera = Camera.main;
+            _obstacle = GetComponentInParent<Obstacle>();
+
+            _currentFillImage.fillAmount = 1f;
+            _resultFillImage.fillAmount = 1f;
+        }
 
         private void OnEnable()
         {
@@ -27,11 +37,12 @@ namespace ObstacleSystem
 
         private void OnHealthChanged(int health)
         {
-            _currentFillImage.fillAmount = (float)health / _obstacle.MaxHealth;
+            float value = (float)health / _obstacle.MaxHealth;
+
+            _currentFillImage.fillAmount = value;
 
             _resultFillImageTween?.Kill();
-
-            _resultFillImageTween = _resultFillImage.DOFillAmount(_currentFillImage.fillAmount, _resultFillDuration)
+            _resultFillImageTween = _resultFillImage.DOFillAmount(value, _resultFillDuration)
                 .SetDelay(_resultFillDelay)
                 .SetEase(_resultFillEase)
                 .SetLink(gameObject);
