d42022f50a5f3f84097cb9a29c4762bac93d7825
OlegPylypchuk2004 <154813576+OlegPylypchuk2004@users.noreply.github.com>
2025-07-03 15:25:59 +0300

    Changed damage number text animation

--- Assets/Game/Scripts/DamageSystem/DamageNumber.cs
+++ Assets/Game/Scripts/DamageSystem/DamageNumber.cs
@@ -6,10 +6,12 @@ namespace DamageSystem
 {
     public class DamageNumber : MonoBehaviour
     {
-        [SerializeField, Min(0f)] private float _duration;
-        [SerializeField, Min(0f)] private float _strength;
+        [SerializeField, Min(0f)] private float _appearDuration;
+        [SerializeField, Min(0f)] private float _punchStrength;
         [SerializeField, Min(0f)] private float _lifeTime;
-        [SerializeField] private Ease _ease;
+        [SerializeField] private Ease _punchEase;
+        [SerializeField, Min(0f)] private float _disappearDuration;
+        [SerializeField] private Ease _disappearEase;
         [SerializeField] private TextMeshPro _textMesh;
 
         public Sequence PlayAnimation(int damage, Color color)
@@ -20,10 +22,13 @@ namespace DamageSystem
             Sequence sequence = DOTween.Sequence();
             sequence.SetLink(gameObject);
 
-            sequence.Append(transform.DOPunchScale(Vector3.one * _strength, _duration, 1)
-                .SetEase(_ease));
+            sequence.Append(transform.DOPunchScale(Vector3.one * _punchStrength, _appearDuration, 1)
+                .SetEase(_punchEase));
 
             sequence.AppendInterval(_lifeTime);
+
+            sequence.Append(transform.DOScale(0f, _disappearDuration)
+                .SetEase(_disappearEase));
 
             sequence.AppendCallback(() =>
             {
