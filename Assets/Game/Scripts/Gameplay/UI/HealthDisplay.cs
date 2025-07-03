bfc153d5a90a78eb797b7705f4dfeea0f6592e69
OlegPylypchuk2004 <154813576+OlegPylypchuk2004@users.noreply.github.com>
2025-07-03 11:45:13 +0300

    Added health display

---
+++ Assets/Game/Scripts/Gameplay/UI/HealthDisplay.cs
@@ -0,0 +1,12 @@
+using UnityEngine;
+
+namespace Gameplay.UI
+{
+    public class HealthDisplay : MonoBehaviour
+    {
+        private void LateUpdate()
+        {
+            transform.rotation = Quaternion.identity;
+        }
+    }
+}
