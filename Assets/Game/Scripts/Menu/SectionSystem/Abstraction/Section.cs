49d4a3cb7d410f9efec918babcb9a0443c9d7656
OlegPylypchuk2004 <154813576+OlegPylypchuk2004@users.noreply.github.com>
2025-08-20 16:26:05 +0300

    Changed menu ui section animation

--- Assets/Game/Scripts/Menu/SectionSystem/Abstraction/Section.cs
+++ Assets/Game/Scripts/Menu/SectionSystem/Abstraction/Section.cs
@@ -7,6 +7,7 @@ namespace Menu.SectionSystem
     {
         [SerializeField] protected SectionChanger _sectionChanger;
         [SerializeField] protected CanvasGroup _canvasGroup;
+        [SerializeField, Min(0f)] protected float _minUIScale;
         [SerializeField, Min(0f)] protected float _appearUIDuration;
         [SerializeField, Min(0f)] protected float _disappearUIDuration;
         [SerializeField] protected Ease _appearUIEase;
@@ -36,6 +37,10 @@ namespace Menu.SectionSystem
                 .From(0f)
                 .SetEase(_appearUIEase));
 
+            sequence.Join(_canvasGroup.transform.DOScale(1f, _appearUIDuration)
+                .From(_minUIScale)
+                .SetEase(_appearUIEase));
+
             sequence.OnComplete(() =>
             {
                 _canvasGroup.interactable = true;
@@ -57,6 +62,10 @@ namespace Menu.SectionSystem
             });
 
             sequence.Append(_canvasGroup.DOFade(0f, _disappearUIDuration)
+                .SetEase(_disappearUIEase));
+
+            sequence.Join(_canvasGroup.transform.DOScale(_minUIScale, _disappearUIDuration)
+                .From(1f)
                 .SetEase(_disappearUIEase));
 
             sequence.OnComplete(() =>
