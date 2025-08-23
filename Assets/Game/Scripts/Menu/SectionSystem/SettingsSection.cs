410df24e95e654196290efc97fd8bcf0214c7bbd
OlegPylypchuk2004 <154813576+OlegPylypchuk2004@users.noreply.github.com>
2025-08-23 18:47:15 +0300

    Added settings buttons

--- Assets/Game/Scripts/Menu/SectionSystem/SettingsSection.cs
+++ Assets/Game/Scripts/Menu/SectionSystem/SettingsSection.cs
@@ -1,5 +1,8 @@
+using SaveSystem;
+using SceneManagment;
 using UnityEngine;
 using UnityEngine.UI;
+using VContainer;
 
 namespace Menu.SectionSystem
 {
@@ -7,20 +10,45 @@ namespace Menu.SectionSystem
     {
         [Space(10f), SerializeField] private Button _backButton;
         [SerializeField] private Section _previousSection;
+        [SerializeField] private Button _secretCodeButton;
+        [SerializeField] private Button _resetProgressButton;
+
+        private SceneLoader _sceneLoader;
+
+        [Inject]
+        private void Construct(SceneLoader sceneLoader)
+        {
+            _sceneLoader = sceneLoader;
+        }
 
         private void OnEnable()
         {
             _backButton.onClick.AddListener(OnBackButtonClicked);
+            _secretCodeButton.onClick.AddListener(OnSecretCodeButtonClicked);
+            _resetProgressButton.onClick.AddListener(OnResetProgressButtonClicked);
         }
 
         private void OnDisable()
         {
             _backButton.onClick.RemoveListener(OnBackButtonClicked);
+            _secretCodeButton.onClick.RemoveListener(OnSecretCodeButtonClicked);
+            _resetProgressButton.onClick.RemoveListener(OnResetProgressButtonClicked);
         }
 
         private void OnBackButtonClicked()
         {
             _sectionChanger.Change(_previousSection);
+        }
+
+        private void OnSecretCodeButtonClicked()
+        {
+
+        }
+
+        private void OnResetProgressButtonClicked()
+        {
+            SaveManager.Delete();
+            _sceneLoader.Load(0);
         }
     }
 }
