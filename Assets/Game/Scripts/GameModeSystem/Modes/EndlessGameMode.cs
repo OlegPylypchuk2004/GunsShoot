c9025e741c61d941fb39761352922d75d351f300
OlegPylypchuk2004 <154813576+OlegPylypchuk2004@users.noreply.github.com>
2025-08-27 13:08:36 +0300

    Update EndlessGameMode.cs

--- Assets/Game/Scripts/GameModeSystem/Modes/EndlessGameMode.cs
+++ Assets/Game/Scripts/GameModeSystem/Modes/EndlessGameMode.cs
@@ -1,22 +1,34 @@
 using Global;
 using SaveSystem;
+using ScoreSystem;
 
 namespace GameModeSystem.Modes
 {
     public class EndlessGameMode : IGameMode
     {
+        private ScoreCounter _scoreCounter;
+
+        public EndlessGameMode(ScoreCounter scoreCounter)
+        {
+            _scoreCounter = scoreCounter;
+        }
+
         public void SaveData()
         {
             string gameModeID = LocalGameData.GameModeConfig.ID;
+            int score = _scoreCounter.Score;
             SaveData saveData = SaveManager.Data;
 
             if (saveData.GameModes.ContainsKey(gameModeID))
             {
-                saveData.GameModes[gameModeID] = 100;
+                if (score > saveData.GameModes[gameModeID])
+                {
+                    saveData.GameModes[gameModeID] = score;
+                }
             }
             else
             {
-                saveData.GameModes.Add(gameModeID, 50);
+                saveData.GameModes.Add(gameModeID, score);
             }
 
             SaveManager.Save();
