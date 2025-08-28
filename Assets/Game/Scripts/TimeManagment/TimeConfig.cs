298efb80dbae2fa85c09e39fec6ad9451041a6d5
OlegPylypchuk2004 <154813576+OlegPylypchuk2004@users.noreply.github.com>
2025-08-28 15:26:40 +0300

    Update TimeConfig.cs

--- Assets/Game/Scripts/TimeManagment/TimeConfig.cs
+++ Assets/Game/Scripts/TimeManagment/TimeConfig.cs
@@ -5,6 +5,6 @@ namespace TimeManagment
     [CreateAssetMenu(fileName = "TimeConfig", menuName = "Configs/Time")]
     public class TimeConfig : ScriptableObject
     {
-        [field: SerializeField] public float SlowDownValue { get; private set; }
+        [field: SerializeField, Range(0f, 1f)] public float SlowDownValue { get; private set; }
     }
 }
