be458e84bdfd26770bdcd230cacb300f6d832457
OlegPylypchuk2004 <154813576+OlegPylypchuk2004@users.noreply.github.com>
2025-08-28 13:42:22 +0300

    Added combo display animation

--- Assets/Game/Scripts/ComboSystem/ComboDisplay.cs
+++ Assets/Game/Scripts/ComboSystem/ComboDisplay.cs
@@ -1,3 +1,4 @@
+using DG.Tweening;
 using ScoreSystem;
 using TMPro;
 using UnityEngine;
@@ -8,17 +9,29 @@ namespace ComboSystem
 {
     public class ComboDisplay : MonoBehaviour
     {
+        [SerializeField] private CanvasGroup _canvasGroup;
         [SerializeField] private TMP_Text _textMesh;
         [SerializeField] private Image _fillImage;
+        [SerializeField] private float _animationDuration;
+        [SerializeField] private float _maxCanvasGroupScale;
+        [SerializeField] private Ease _appearEase;
+        [SerializeField] private Ease _disappearEase;
 
         private ComboCounter _comboCounter;
         private ScoreCounter _scoreCounter;
+        private Sequence _currentSequence;
+        private bool _isActive;
 
         [Inject]
         private void Construct(ComboCounter comboCounter, ScoreCounter scoreCounter)
         {
             _comboCounter = comboCounter;
             _scoreCounter = scoreCounter;
+        }
+
+        private void Awake()
+        {
+            _canvasGroup.alpha = 0f;
         }
 
         private void OnEnable()
@@ -35,15 +48,71 @@ namespace ComboSystem
 
         private void OnComboChanged(int combo)
         {
-            float scoreMultiplier = _scoreCounter.Multiplier;
-            string formattedScoreMultiplier = scoreMultiplier.ToString(System.Globalization.CultureInfo.InvariantCulture);
+            if (combo >= 1)
+            {
+                float scoreMultiplier = _scoreCounter.Multiplier;
+                string formattedScoreMultiplier = scoreMultiplier.ToString(System.Globalization.CultureInfo.InvariantCulture);
 
-            _textMesh.text = $"x{formattedScoreMultiplier}";
+                _textMesh.text = $"x{formattedScoreMultiplier}";
+            }
+
+            if (!_isActive)
+            {
+                _isActive = true;
+
+                Appear();
+            }
         }
 
         private void OnTimeChanged(float time)
         {
             _fillImage.fillAmount = time / 5f;
+
+            if (time <= 0f)
+            {
+                if (_isActive)
+                {
+                    _isActive = false;
+
+                    Disappear();
+                }
+            }
+        }
+
+        private Sequence Appear()
+        {
+            _currentSequence?.Kill();
+
+            _currentSequence = DOTween.Sequence();
+            _currentSequence.SetLink(gameObject);
+
+            _currentSequence.Append(_canvasGroup.transform.DOScale(1f, _animationDuration)
+                .From(_maxCanvasGroupScale)
+                .SetEase(_disappearEase));
+
+            _currentSequence.Join(_canvasGroup.DOFade(1f, _animationDuration)
+                .From(0f)
+                .SetEase(_appearEase));
+
+            return _currentSequence;
+        }
+
+        private Sequence Disappear()
+        {
+            _currentSequence?.Kill();
+
+            _currentSequence = DOTween.Sequence();
+            _currentSequence.SetLink(gameObject);
+
+            _currentSequence.Append(_canvasGroup.transform.DOScale(_maxCanvasGroupScale, _animationDuration)
+                .From(1f)
+                .SetEase(_disappearEase));
+
+            _currentSequence.Join(_canvasGroup.DOFade(0f, _animationDuration)
+                .From(1f)
+                .SetEase(_disappearEase));
+
+            return _currentSequence;
         }
     }
 }
