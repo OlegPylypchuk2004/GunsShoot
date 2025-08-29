8e160a80a7dc92d9a68b49225295a2d98e46dea9
OlegPylypchuk2004 <154813576+OlegPylypchuk2004@users.noreply.github.com>
2025-08-29 16:15:03 +0300

    Update HealthDisplay.cs

--- Assets/Game/Scripts/Gameplay/UI/HealthDisplay.cs
+++ Assets/Game/Scripts/Gameplay/UI/HealthDisplay.cs
@@ -1,5 +1,4 @@
 using HealthSystem;
-using System.Collections;
 using TMPro;
 using UnityEngine;
 using UnityEngine.UI;
@@ -11,22 +10,13 @@ namespace Gameplay.UI
     {
         [SerializeField] private TextMeshProUGUI _textMesh;
         [SerializeField] private Image _icon;
-        [SerializeField] private Color _shineIconColor;
-        [SerializeField, Min(0f)] private float _shineDuration;
 
         private HealthManager _healthManager;
-        private Coroutine _iconAnimationCoroutine;
-        private Color _defaultIconColor;
 
         [Inject]
         private void Construct(HealthManager healthManager)
         {
             _healthManager = healthManager;
-        }
-
-        private void Awake()
-        {
-            _defaultIconColor = _icon.color;
         }
 
         private void OnEnable()
@@ -49,28 +39,11 @@ namespace Gameplay.UI
         private void OnHealthChanged(int health)
         {
             UpdateText(health);
-
-            if (_iconAnimationCoroutine != null)
-            {
-                StopCoroutine(_iconAnimationCoroutine);
-                _iconAnimationCoroutine = null;
-            }
-
-            _iconAnimationCoroutine = StartCoroutine(PlayIconAnimationCoroutine());
         }
 
         private void UpdateText(int health)
         {
             _textMesh.text = $"x{health}";
-        }
-
-        private IEnumerator PlayIconAnimationCoroutine()
-        {
-            _icon.color = _shineIconColor;
-
-            yield return new WaitForSeconds(_shineDuration);
-
-            _icon.color = _defaultIconColor;
         }
     }
 }
