03527889e89a58194f68dd8fbeeb449f737b5a19
OlegPylypchuk2004 <154813576+OlegPylypchuk2004@users.noreply.github.com>
2025-09-26 15:37:29 +0300

    Fixed levels mode system

--- Assets/Game/Scripts/GameModeSystem/LevelGameModeConfig.cs
+++ Assets/Game/Scripts/GameModeSystem/LevelGameModeConfig.cs
@@ -6,6 +6,8 @@ namespace GameModeSystem
     [CreateAssetMenu(fileName = "GameModeConfig", menuName = "Configs/GameModes/Level")]
     public class LevelGameModeConfig : GameModeConfig
     {
+        [field: SerializeField, Min(1)] public int LevelsCount { get; private set; }
+
         public override string GetStagesConfigFilePath()
         {
             SaveData saveData = SaveManager.Data;
@@ -13,6 +15,12 @@ namespace GameModeSystem
             if (saveData.GameModes.ContainsKey(ID))
             {
                 int levelNumber = saveData.GameModes[ID];
+
+                if (levelNumber > LevelsCount)
+                {
+                    levelNumber = LevelsCount;
+                }
+
                 string path = $"{StagesConfigFilePath}{levelNumber}.json";
 
                 return path;
