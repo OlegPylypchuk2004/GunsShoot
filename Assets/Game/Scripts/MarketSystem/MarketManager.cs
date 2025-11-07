70d79a4406e8778472519bb475be55362aec383a
OlegPylypchuk2004 <154813576+OlegPylypchuk2004@users.noreply.github.com>
2025-11-07 12:37:51 +0200

    Added increase market rewards

---
+++ Assets/Game/Scripts/MarketSystem/MarketManager.cs
@@ -0,0 +1,45 @@
+using CurrencyManagment;
+using UnityEngine;
+using VContainer;
+
+namespace MarketSystem
+{
+    public class MarketManager : MonoBehaviour
+    {
+        [SerializeField] private CurrencyConfig _currencyConfig;
+
+        private CurrencyWallet _currencyWallet;
+
+        [Inject]
+        private void Construct(CurrencyWallet currencyWallet)
+        {
+            _currencyWallet = currencyWallet;
+        }
+
+        public void OnOrderConfirmed(string itemID)
+        {
+            switch (itemID)
+            {
+                case "starter_pack":
+                    _currencyWallet.TryIncrease(new WalletOperationData(_currencyConfig, 500));
+                    break;
+
+                case "medium_pack":
+                    _currencyWallet.TryIncrease(new WalletOperationData(_currencyConfig, 2750));
+                    break;
+
+                case "big_pack":
+                    _currencyWallet.TryIncrease(new WalletOperationData(_currencyConfig, 5500));
+                    break;
+
+                case "mega_pack":
+                    _currencyWallet.TryIncrease(new WalletOperationData(_currencyConfig, 8250));
+                    break;
+
+                case "ultra_pack":
+                    _currencyWallet.TryIncrease(new WalletOperationData(_currencyConfig, 12500));
+                    break;
+            }
+        }
+    }
+}
