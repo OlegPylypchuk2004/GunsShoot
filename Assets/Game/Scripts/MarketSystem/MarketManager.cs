3bed4169ba2b1ad1ffb905d3e5bfef6b76ebb3e6
OlegPylypchuk2004 <154813576+OlegPylypchuk2004@users.noreply.github.com>
2025-11-07 14:13:07 +0200

    Changed iap settings

--- Assets/Game/Scripts/MarketSystem/MarketManager.cs
+++ Assets/Game/Scripts/MarketSystem/MarketManager.cs
@@ -20,23 +20,23 @@ namespace MarketSystem
         {
             switch (itemID)
             {
-                case "starter_pack_1":
+                case "starter_pack_2025":
                     _currencyWallet.TryIncrease(new WalletOperationData(_currencyConfig, 500));
                     break;
 
-                case "medium_pack_1":
+                case "medium_pack_2025":
                     _currencyWallet.TryIncrease(new WalletOperationData(_currencyConfig, 2750));
                     break;
 
-                case "big_pack_1":
+                case "big_pack_2025":
                     _currencyWallet.TryIncrease(new WalletOperationData(_currencyConfig, 5500));
                     break;
 
-                case "mega_pack_1":
+                case "mega_pack_2025":
                     _currencyWallet.TryIncrease(new WalletOperationData(_currencyConfig, 8250));
                     break;
 
-                case "ultra_pack_1":
+                case "ultra_pack_2025":
                     _currencyWallet.TryIncrease(new WalletOperationData(_currencyConfig, 12500));
                     break;
             }
