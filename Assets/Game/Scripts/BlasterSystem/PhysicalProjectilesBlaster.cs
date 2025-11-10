d66f5d85b4b9d1b4ad56032ca884973f1a0856d8
OlegPylypchuk2004 <154813576+OlegPylypchuk2004@users.noreply.github.com>
2025-11-10 19:17:47 +0200

    Changed blaster i shoot system

--- Assets/Game/Scripts/BlasterSystem/PhysicalProjectilesBlaster.cs
+++ Assets/Game/Scripts/BlasterSystem/PhysicalProjectilesBlaster.cs
@@ -5,6 +5,10 @@ namespace BlasterSystem
 {
     public class PhysicalProjectilesBlaster : Blaster
     {
+        [SerializeField] private bool _isSalvo;
+
+        private int _shootPointIndex;
+
         private void Awake()
         {
             _projectilesManager = new PhysicalProjectilesManager();
@@ -20,17 +24,39 @@ namespace BlasterSystem
         {
             Vector3[] projectilesDirections = GetProjectilesDirections();
 
-            for (int i = 0; i < projectilesDirections.Length; i++)
+            if (_isSalvo)
+            {
+                for (int i = 0; i < projectilesDirections.Length; i++)
+                {
+                    PhysicalProjectileData projectileData = new PhysicalProjectileData();
+                    projectileData.Damage = Config.Damage;
+                    projectileData.Direction = projectilesDirections[i];
+                    projectileData.Speed = Config.ProjectileSpeed;
+
+                    PhysicalProjectile projectile = (PhysicalProjectile)_projectilesManager.CreateProjectile();
+                    projectile.transform.position = _shootPoints[i].position;
+                    projectile.SetRigidbodyPosition(_shootPoints[i].position);
+                    projectile.Launch(projectileData);
+                }
+            }
+            else
             {
                 PhysicalProjectileData projectileData = new PhysicalProjectileData();
                 projectileData.Damage = Config.Damage;
-                projectileData.Direction = projectilesDirections[i];
+                projectileData.Direction = projectilesDirections[_shootPointIndex];
                 projectileData.Speed = Config.ProjectileSpeed;
 
                 PhysicalProjectile projectile = (PhysicalProjectile)_projectilesManager.CreateProjectile();
-                projectile.transform.position = _shootPoints[i].position;
-                projectile.SetRigidbodyPosition(_shootPoints[i].position);
+                projectile.transform.position = _shootPoints[_shootPointIndex].position;
+                projectile.SetRigidbodyPosition(_shootPoints[_shootPointIndex].position);
                 projectile.Launch(projectileData);
+
+                _shootPointIndex++;
+
+                if (_shootPointIndex >= _shootPoints.Length)
+                {
+                    _shootPointIndex = 0;
+                }
             }
         }
     }
